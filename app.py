e1cdfdb43a31e7b122f0a0cd624bad93a9e787f7
Hemant <138124475+yhemant407@users.noreply.github.com>
2026-02-01 18:55:46 +0530

    Fix typo in print statement

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("I am just testing. To learn jenkins.")
 print("making some changes in the file")
 print("testing poll SCM, to see if a new build will be trigerred if changes are made.")
-Print("creating error on purpose")
+print("creating error on purpose")
